61e7132d7f0132e887f88a81f6005c3e7991607d
Daniel Kean <jellymoongames@gmail.com>
2020-05-14 07:15:00 +0100

    Add 'Microphone' model contents and add deeper understanding text.

--- src/application/view/viewMain.php
+++ src/application/view/viewMain.php
@@ -231,13 +231,109 @@
             </p>
 
             <h5>Blender</h5>
-            <p class="justify">I used Blender.</p>
-
-            <h5>Carousel</h5>
-            <p class="justify">I implemented a carosuel.</p>
+            <p class="justify">
             
+                For the 3D authoring aspect of this assignment, I utilised Blender, which is a free, open source toolset to model, animate and light 3D scenes.
+                As Blender was not taught in the labs, I had to teach myself the workflow of the program and how to create the same output as 3ds Max.
+                One issue that I had to overcome was that there was a bug in the more recent versions of Blender which prevented x3d models being exported out with texture data,
+                which meant that I would be unable to display textured models in my app and only show a diffuse colour. However after some debugging, I found that I had to
+                manually add the ‘ImageTexture’ and ‘TextureTransform’ tags in-between the ‘Appearance’ tag inside the x3d file, which ended up creating a link to the texture and applying it to the UV-unwrapped model.
+
+            </p>
+
+            <h5>PBR Rendering</h5>
+            <p class="justify">
+
+                For the 3D images, I used Blender’s ‘Eevee’ renderer which utilises physically-based Rendering (PBR) materials which allows for photorealistic results.
+                This means that for each material, I was able to adjust settings such as how metallic it was, how specular it was, or how rough the surface was.
+                This gives you an essentially unlimited amount of control when it comes to material authoring, which is why it’s used heavily the art industry.
+                In the settings for the renderer, I added various visual effects that increase the realism of the image, such as ambient occlusion, bloom,
+                screen-space reflections and baked-indirect lighting from an irradiance volume.
+
+            </p>
+            
+            <h5>Carousel</h5>
+            <p class="justify">
+
+                On my home page, I implemented an image carousel that automatically slides between images and has an indicator to show which image you are currently on.
+                It also has interactive buttons which allows you to go either backwards or forward to select an image, providing extra interaction for the user.
+                As this was not taught in the lab, I had to learn the classes and structure of the carousel component.
+
+            </p>
+
+            <h5>Gallery Hover System</h5>
+            <p class="justify">
+
+                To manage the 3D models for the app, I decided to use a gallery which contains an entry for each model that is contained in the database.
+                Extra functionality that I implemented that was not taught in the labs was to add various effects when the user hovers over an item in the gallery.
+                For example, one of the effects that I added was that the image has a blur transition. Another effect when hovering was that the image slightly loses its colour
+                by applying a weak grayscale filter to the image, which may make it easier to see the text if it happens to remove strong, intense colours.
+                Another effect that I added was to zoom in the image slightly to add a bit more interaction to the app and to make it obvious when the user is hovering over an item.
+                The last effect that I added was to fade in a semi-transparent text banner which contains the name of the model and a button that fades in an orange glow
+                around the border whose purpose is to open up the modal component. The background image and name of the model shown is all loaded in from the database
+                and is a dynamic system which could handle hundreds of models automatically, making it easier to maintain the app as you only need to add an entry to the
+                database and don’t need to change the source code of the website, which could lead to human error and bugs.
+
+            </p>
+
             <h5>Modal System</h5>
-            <p class="justify">I implemented a modal pop-up system for displaying the models.</p>  
+            <p class="justify">
+            
+                To actually view the models for my app, I decided to use a modal system from the bootstrap framework, which essentially acts as a pop-up and is displayed over the current page that you are on. 
+                As this was not taught in the labs, I had to learn the intricacies of the framework and adapt it to suit that I functionality that I wanted.
+                In my implementation, there is only one ‘global’ modal component that exists in the app, which makes it an efficient system as it means that you wouldn’t
+                have multiple x3d components contained in the app at the same time which would decrease performance. For example, even if you had 100 models on your website,
+                then my system would only have one x3d canvas at any given time, making it much more efficient. Every time a user clicks on an item in the gallery,
+                it calls a JavaScript function that updates the modal in the app with the necessary information, that it pulls from the database. It knows which model information
+                to get as every item passes in a unique ID which corresponds with the ID primary key contained in the database. The elements that are updated in the modal component
+                are the header, the 3d model, the interactivity buttons such as the camera angle and light buttons, the model description, and the 3d images of the model.
+
+            </p>
+
+            <h5>Animation Control</h5>
+            <p class="justify">
+
+                To add more interaction to the app, I decided to implement the ability for the user to change the speed of the animation when it plays.
+                I did this by changing the ‘cycleInterval’ parameter of the ‘TimeSensor’ for each of the models. As I have used a generalised approach by using only
+                one modal and then swapping the models out dynamically, it allows me to use the same names for the model objects in the x3d file meaning that I don’t
+                need to do any specific work to connect the buttons to each individual model. Along with changing the animation speed, the interaction section also has
+                a button for toggling all the lights in each scene. It does this by looping through and finding all the lights that exist, meaning that this system works
+                with any number of lights if needed (although I limited it to a maximum of 7 as I knew that was the most I had in any model).
+
+            </p>
+
+            <h5>Deep Image AI</h5>
+            <p class="justify">
+
+                For the model’s textures, I utilised the ‘Deep Image AI’ technology which utilises machine learning to upscale images into a higher quality.
+                This website allowed me to achieve higher quality models as the textures are clearer, which especially benefits textures that include text.
+                This technique was used for the Coca-Cola Can and Fanta Bottle and made a quite significant difference.
+
+            </p>
+
+            <h5>Photoshop</h5>
+            <p class="justify">
+
+                For the various art that I made for the website, such as the carousel contents and the images seen, I used photoshop to adjust them and make sure
+                they were in the right format (JPEG) and that they were the right dimensions, both important steps when including such media on a
+                website as you want small file sizes and consistency throughout.
+
+            </p>
+
+            <h5>JQuery</h5>
+            <p class="justify">
+
+                For the JavaScript in my app, I utilised the highly popular JQuery framework to make to make the more tedious tasks of JavaScript
+                easier to program then if I were to just use the standard JavaScript libraries.
+
+            </p>
+
+            <h5>Bootstrap</h5>
+            <p class="justify">
+
+                For my front-end framework, I utilised Bootstrap 4 and the various components that it provides. The use of these components is detailed in the other sections of this page.
+
+            </p>
 
         </div>
 
